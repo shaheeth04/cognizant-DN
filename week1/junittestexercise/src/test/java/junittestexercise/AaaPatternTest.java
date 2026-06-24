414d7b6a351dda734d487bb2591eccaaf6a75658
Shaheeth Hussain M <shaheethussain@gmail.com>
2026-06-24 08:26:59 +0530

    Add AAA pattern JUnit exercise

---
+++ week1/junittestexercise/src/test/java/junittestexercise/AaaPatternTest.java
@@ -0,0 +1,37 @@
+package junittestexercise;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class AaaPatternTest {
+
+    private int number;
+
+    @Before
+    public void setUp() {
+        number = 10;
+    }
+
+    @After
+    public void tearDown() {
+        number = 0;
+    }
+
+    @Test
+    public void testAdditionWithArrangeActAssert() {
+        // Arrange
+        int firstValue = 5;
+        int secondValue = 3;
+
+        // Act
+        int result = firstValue + secondValue;
+
+        // Assert
+        assertEquals(8, result);
+        assertTrue(number > 0);
+    }
+}
